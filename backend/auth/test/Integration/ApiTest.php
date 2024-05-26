096b466353408109d3c1b7b5ef8e04eeee655c9a
Thomas Moraes <tho.vini7@gmail.com>
2024-05-26 19:12:49 -0300

    fix: Removendo teste de api

--- backend/auth/test/Integration/ApiTest.php
+++ backend/auth/test/Integration/ApiTest.php
@@ -18,11 +18,3 @@ test("Deve testar o fluxo de autenticação", function() {
     $output = json_decode($response->getBody());
     expect("Email already in use")->toBe($output->errors->message);
 });
-
-test("Deve testar a verificação de token", function() {
-    $input = ["token" => "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhdXRoIiwiaWF0IjoxNzE2NzU0MDU0LCJleHAiOjE3MTY3NTc2NTQsImVtYWlsIjoidGhvbWFzQGdtYWlsLmNvbSJ9.e6cdqqrumpRR9HEj8kJ70iy40Xe2pXkZziO5perDPSY"];
-    $response = $this->client->post("verify", $input);
-    $output = json_decode($response->getBody());
-    expect($response->getStatusCode())->toBe(200);
-    expect($output->email)->toBe("thomas@gmail.com");
-});
