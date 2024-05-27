2ba5ef21cf665bed86f3b487795441f00a0ba286
Thomas Moraes <tho.vini7@gmail.com>
2024-05-27 12:46:36 -0300

    fix: Testando Api de Produtos

---
+++ backend/catalog/test/Integration/ApiTest.php
@@ -0,0 +1,12 @@
+<?php 
+namespace Test\Integration;
+
+use Auth\Infra\Http\Client\GuzzleAdapter;
+
+test("Deve listar os produtos", function() {
+    $client = new GuzzleAdapter("http://localhost:8008");
+    $response = $client->get("products");
+    $output = json_decode($response->getBody());
+    expect($response->getStatusCode())->toBe(200);
+    expect(count($output))->toBe(2);
+});
